4894a01cfbbb5be3629c75d75fa687901f062eff
RallaPirate <tantegwen@gmail.com>
2025-02-05 15:46:06 +0100

    eeste-commit

---
+++ src/test/java/nl/spplatform/sppapi/SppapiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.spplatform.sppapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SppapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
