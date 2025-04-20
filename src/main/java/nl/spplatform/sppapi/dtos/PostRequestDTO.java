8b5afedb1df8afaf157ae0367e9b0208ff8746ff
RallaPirate <tantegwen@gmail.com>
2025-04-20 22:12:56 +0200

    PostRequestDTO geschreven

--- src/main/java/nl/spplatform/sppapi/dtos/PostRequestDTO.java
+++ src/main/java/nl/spplatform/sppapi/dtos/PostRequestDTO.java
@@ -1,4 +1,35 @@
 package nl.spplatform.sppapi.dtos;
 
 public class PostRequestDTO {
+
+    private String title;
+    private String region;
+    private String posttext;
+    private Long userId;
+
+    public String getTitle(){
+        return title;
+    }
+    public String getRegion(){
+        return region;
+    }
+    public String getPosttext(){
+        return posttext;
+    }
+    public Long getUserId(){
+        return userId;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setRegion(String region){
+        this.region = region;
+    }
+    public void setPosttext(String posttext){
+        this.posttext = posttext;
+    }
+    public void setUserId(Long userId){
+        this.userId = userId;
+    }
 }
