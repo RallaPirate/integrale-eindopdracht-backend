b53556c169b05482cbbb1efe3f5497511ba697b4
RallaPirate <tantegwen@gmail.com>
2025-04-22 14:19:29 +0200

    Getroubleshoot om mijn data.sql in mijn database te krijgen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (username, password, role) VALUES ('user', $2a$10$ZpMvZHODyqVWJHNjCNyCV.zatfk5t.6T/n1n11VyV1V9ZCizsQU36, 'USER');
+INSERT INTO users (username, password, role) VALUES ('user', '$2a$10$ZpMvZHODyqVWJHNjCNyCV.zatfk5t.6T/n1n11VyV1V9ZCizsQU36', 'USER');
